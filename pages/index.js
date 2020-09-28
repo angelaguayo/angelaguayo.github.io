26a5a49d588e322c2a89bbe874a6359a6dfeb564
angelaguayo <anggelagguayoo@gmail.com>
2020-09-28 13:53:09 -0500

    tailwind works tailwind its worlks

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function IndexPage() {
   return (
-    <div>
-      Hello World.{' '}
+    <div className="text-2x1 text-blue-700">
+      Hello World.{" "}
       <Link href="/about">
         <a>About</a>
       </Link>
     </div>
-  )
+  );
 }
